8be3da7c872cd00d2f26d3f81921b595c7986aef
Dennis <dennismasaya@gmail.com>
2019-10-06 16:27:37 -0600

    CPU module complete

--- common/Handlers.go
+++ common/Handlers.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+  "strconv"
+  "io/ioutil"
+  "strings"
   "fmt"
   "net/http"
   "encoding/json"
@@ -69,13 +72,47 @@ func RamData(response http.ResponseWriter, request *http.Request){
 
 //======= CPU DATA
 
+func getCPUSample() (idle, total uint64) {
+    contents, err := ioutil.ReadFile("/proc/stat")
+    if err != nil {
+        return
+    }
+    lines := strings.Split(string(contents), "\n")
+    for _, line := range(lines) {
+        fields := strings.Fields(line)
+        if fields[0] == "cpu" {
+            numFields := len(fields)
+            for i := 1; i < numFields; i++ {
+                val, err := strconv.ParseUint(fields[i], 10, 64)
+                if err != nil {
+                    fmt.Println("Error: ", i, fields[i], err)
+                }
+                total += val // tally up all the numbers to get total ticks
+                if i == 4 {  // idle is the 5th field in the cpu line
+                    idle = val
+                }
+            }
+            return
+        }
+    }
+    return
+}
+
 type CpuStruct struct{
   X int `json:"x"`
   Y int `json:"y"`
 }
 
 func CpuData(response http.ResponseWriter, request *http.Request){
-  cpu_data := RamStruct{X:rand.Intn(100),Y:rand.Intn(100)}
+  idle0, total0 := getCPUSample()
+  time.Sleep(3 * time.Second)
+  idle1, total1 := getCPUSample()
+
+  idleTicks := float64(idle1 - idle0)
+  totalTicks := float64(total1 - total0)
+  cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+
+  cpu_data := RamStruct{X:int(totalTicks-idleTicks),Y:int(cpuUsage)}
   byteArray, _ := json.Marshal(cpu_data)
   fmt.Fprintf(response,string(byteArray))
 }
