6510ec8ae51a7d217c98f41ea74c6239f3afda00
Dennis <dennismasaya@gmail.com>
2019-10-06 12:01:38 -0600

    web-server working correctly

--- common/Handlers.go
+++ common/Handlers.go
@@ -3,14 +3,58 @@ package handlers
 import (
   "fmt"
   "net/http"
+  "encoding/json"
+  "time"
+  "math/rand"
+  "github.com/thedevsaddam/renderer"
   helpers "../helpers"
   repos "../repos"
 )
+
+var rnd *renderer.Render
+
+func init() {
+	opts := renderer.Options{
+		ParseGlobPattern: "./static/*.html",
+	}
+
+	rnd = renderer.New(opts)
+}
+
+
+func CpuProcessHandler(response http.ResponseWriter, request *http.Request){
+  rnd.HTML(response, http.StatusOK, "cpu_graph", nil)
+}
+
+func RamProcessHandler(response http.ResponseWriter, request *http.Request){
+  rnd.HTML(response, http.StatusOK, "ram_graph", nil)
+}
+
+
+func AdminPageHandler(response http.ResponseWriter, request *http.Request){
+  rnd.HTML(response, http.StatusOK, "admin", nil)
+  //var body, _= helpers.LoadFile("static/chartjs_example3.html")
+  //fmt.Fprintf(response, body)
+}
 
 //LoginPage GET
 func LoginPageHandler(response http.ResponseWriter, request *http.Request){
   var body, _= helpers.LoadFile("static/index.html")
   fmt.Fprintf(response, body)
+}
+
+type Values struct{
+  X string`json:"x"`
+  Y int `json:"y"`
+}
+
+func AdminHandler(response http.ResponseWriter, request *http.Request){
+
+  values := Values{X:time.Now().Format("2006-01-02T15:04:05Z"),Y:rand.Intn(100)}
+
+  byteArray, _ := json.Marshal(values)
+
+  fmt.Fprintf(response,string(byteArray))
 }
 
 //LoginPage POST
--- server.go
+++ server.go
@@ -18,25 +18,23 @@ func NewRouter() *mux.Router {
     router := mux.NewRouter().StrictSlash(true)
 		router.PathPrefix("/css/").Handler(http.StripPrefix("/css/",
 		http.FileServer(http.Dir("static/css/"))))
-    // Server CSS, JS & Images Statically.
-    //router.
-    //    PathPrefix(STATIC_DIR).
-    //    Handler(http.StripPrefix(STATIC_DIR, http.FileServer(http.Dir("."+STATIC_DIR))))
-
     return router
 }
 
 func main() {
 
-	//cssHandler := http.FileServer(http.Dir("./static/css/"))
-	//http.Handle("/static/css/", http.StripPrefix("/static/css/", cssHandler))
+	//====== GET METHODS
+	router.HandleFunc("/", common.LoginPageHandler)
+	router.HandleFunc("/adminPage",common.AdminPageHandler)
+	router.HandleFunc("/cpu_graph", common.CpuProcessHandler)
+	router.HandleFunc("/ram_graph", common.RamProcessHandler)
 
-
-
-	router.HandleFunc("/", common.LoginPageHandler) // GET
+	//====== POST METHODS
 	router.HandleFunc("/login", common.LoginHandler).Methods("POST")
+	router.HandleFunc("/data",common.AdminHandler).Methods("POST")
+
+	//====== CONFIG
 	http.Handle("/",router)
 	log.Println("Server running on http://localhost:8080")
   log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
